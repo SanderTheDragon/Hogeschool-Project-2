a6cc8f26e647214064efe8e55b64e853f203796e
gapjustin <vuilnisbuilt@gmail.com>
2017-01-10 13:35:29 +0100

    Le update

--- Code.c
+++ Code.c
@@ -1,5 +1,7 @@
 void setup() {
-  
+    for (int i = 4; i < 8; i++){
+        pinMode(i, OUTPUT);
+    }
 }
 
 void loop() {
@@ -16,7 +18,12 @@ void goRight(){
 
 
 void driveForwards(){
-
+    digitalWrite(7, HIGH);
+    delayMicroseconds(2500);
+    digitalWrite(7, LOW);
+    digitalWrite(6, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(6, LOW);  
 }
 
 void driveBackwards(){
