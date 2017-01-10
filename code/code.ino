a8e7eef0a8c741bba9e8c60c2a27bc69efad570f
SanderTheDragon <sanderthedragon@zoho.com>
2017-01-10 16:25:10 +0100

    Code voor de stepper motor, om ooit te testen

--- code/code.ino
+++ code/code.ino
@@ -1,7 +1,18 @@
+#include <Stepper.h>
+
 const int sensor1Trigger = 2;
 const int sensor1Echo = 3;
 const int sensor2Trigger = 4;
 const int sensor2Echo = 5;
+
+const int stepper1Pins[4] = { 6, 7, 8, 9 };
+const int stepper2Pins[4] = { 10, 11, 12, 13 };
+
+const int stepsPerMotorRev = 32;
+const int stepsPerOutputRev = stepsPerMotorRev * 64;
+
+Stepper stepper1(stepsPerMotorRev, stepper1Pins[0], stepper1Pins[1], stepper1Pins[2], stepper1Pins[3]);
+Stepper stepper2(stepsPerMotorRev, stepper2Pins[0], stepper2Pins[1], stepper2Pins[2], stepper2Pins[3]);
 
 void setup()
 {
@@ -62,23 +73,43 @@ int getDistance2()
   return distance;
 }
 
+void motorLeft(int direction, int time = 1000)
+{
+  int steps = stepsPerOutputRev * direction;
+  stepper1.setSpeed(640);
+  stepper1.step(steps);
+  delay(time);
+}
+
+void motorRight(int direction, int time = 1000)
+{
+  int steps = stepsPerOutputRev * direction;
+  stepper2.setSpeed(640);
+  stepper2.step(steps);
+  delay(time);
+}
+
 void goLeft()
 {
-  
+  motorLeft(-1);
+  motorRight(1);
 }
 
 void goRight()
 {
-  
+  motorLeft(1);
+  motorRight(-1);
 }
 
 void driveForwards()
 {
-  
+  motorLeft(1);
+  motorRight(1);
 }
 
 void driveBackwards()
 {
-  
+  motorLeft(-1);
+  motorRight(-1);
 }
 
