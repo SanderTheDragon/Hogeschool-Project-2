205df9567e414c9439415e8d2411996d092700c3
SanderTheDragon <sanderthedragon@zoho.com>
2017-01-10 13:38:03 +0100

    code is weer goed

--- Code.c
+++ Code.c
@@ -1,10 +1,9 @@
-void setup() {
-    for (int i = 4; i < 8; i++){
-        pinMode(i, OUTPUT);
-    }
 void setup()
 {
-    
+    for (int i = 4; i < 8; i++)
+    {
+        pinMode(i, OUTPUT);
+    }
 }
 
 void loop()
@@ -22,17 +21,14 @@ void goRight()
     
 }
 
-
-void driveForwards(){
+void driveForwards()
+{
     digitalWrite(7, HIGH);
     delayMicroseconds(2500);
     digitalWrite(7, LOW);
     digitalWrite(6, HIGH);
     delayMicroseconds(500);
-    digitalWrite(6, LOW);  
-void driveForwards()
-{
-    
+    digitalWrite(6, LOW);    
 }
 
 void driveBackwards()
