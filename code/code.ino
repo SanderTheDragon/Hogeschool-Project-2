0c06656a3cc989518c0858b6d5099eb483d09a81
SanderTheDragon <sanderthedragon@zoho.com>
2017-01-17 14:48:42 +0100

    Werkende code :D

--- code/code.ino
+++ code/code.ino
@@ -1,3 +1,4 @@
+#include <NewPing.h>
 #include <CustomStepper.h>
 
 const int sensor1Trigger = 2;
@@ -11,10 +12,14 @@ const int stepper2Pins[4] = { 10, 11, 12, 13 };
 CustomStepper stepper1(stepper1Pins[0], stepper1Pins[1], stepper1Pins[2], stepper1Pins[3], (byte[]){8, B1000, B1100, B0100, B0110, B0010, B0011, B0001, B1001});
 CustomStepper stepper2(stepper2Pins[0], stepper2Pins[1], stepper2Pins[2], stepper2Pins[3], (byte[]){8, B1000, B1100, B0100, B0110, B0010, B0011, B0001, B1001});
 
+NewPing sensor1(sensor1Trigger, sensor1Echo, 200);
+NewPing sensor2(sensor2Trigger, sensor2Echo, 200);
+
+int distance1 = 0, distance2 = 0;
+bool reqDistance = true;
+
 void setup()
 {
-  //Serial.begin(9600);
-
   stepper1.setRPM(15);
   stepper1.setSPR(4075.7728395);
   
@@ -29,140 +34,47 @@ void setup()
 
 void loop()
 {
-  if (getDistance1() < 10)
+  if (reqDistance)
   {
-    driveStop();
-    driveBackwards();
+    distance1 = sensor1.ping_cm();
+    distance2 = sensor2.ping_cm();
+
+    if (distance1 < 10)
+    {
+      stepper1.setDirection(CCW);
+      stepper2.setDirection(CW);
+      
+      stepper1.rotateDegrees(5);
+      stepper2.rotateDegrees(5);
+    }
+    else
+    {
+      if (distance2 > 10)
+      {
+        stepper1.setDirection(CW);
+        stepper2.setDirection(CW);
+        
+        stepper1.rotateDegrees(5);
+        stepper2.rotateDegrees(5);
+      }
+      else
+      {
+        stepper1.setDirection(CW);
+        stepper2.setDirection(CCW);
+        
+        stepper1.rotateDegrees(5);
+        stepper2.rotateDegrees(5);
+      }
+    }
+
+    reqDistance = false;
   }
-  else
+
+  if (stepper1.isDone() && stepper2.isDone())
   {
-    driveStop();
-    driveForwards();
+    reqDistance = true;
   }
   
   stepper1.run();
   stepper2.run();
 }
-
-int getDistance1()
-{
-  digitalWrite(sensor1Trigger, LOW);
-  delayMicroseconds(2);
-  digitalWrite(sensor1Trigger, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(sensor1Trigger, LOW);
-  
-  int duration = pulseIn(sensor1Echo, HIGH);
-  int distance = duration / 58.2;
-  
-  if (distance > 200 || distance < 0)
-    distance = -1;
-  
-  //Serial.print("Sensor 1: ");
-  //Serial.print(distance);
-  //Serial.print("\n");
-
-  return distance;
-}
-
-int getDistance2()
-{
-  digitalWrite(sensor2Trigger, LOW);
-  delayMicroseconds(2);
-  digitalWrite(sensor2Trigger, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(sensor2Trigger, LOW);
-  
-  int duration = pulseIn(sensor2Echo, HIGH);
-  int distance = duration / 58.2;
-  
-  if (distance > 200 || distance < 0)
-    distance = -1;
-  
-  //Serial.print("Sensor 2: ");
-  //Serial.print(distance);
-  //Serial.print("\n");
-
-  return distance;
-}
-
-void motorLeft(int direction)
-{
-  if (stepper1.isDone())
-  {
-    //Serial.print("Motor left: ");
-    //Serial.print(direction);
-    //Serial.print("\n");
-    
-    stepper1.setDirection(CW);
-    
-    if (direction < 0)
-      stepper1.setDirection(CCW);
-
-    if (direction == 0)
-    {
-      stepper1.setDirection(STOP);
-      return;
-    }
-    
-    stepper1.rotate(1);
-  }
-}
-
-void motorRight(int direction)
-{
-  if (stepper2.isDone())
-  {
-    //Serial.print("Motor right: ");
-    //Serial.print(direction);
-    //Serial.print("\n");
-    
-    stepper2.setDirection(CW);
-    
-    if (direction < 0)
-      stepper2.setDirection(CCW);
-    
-    if (direction == 0)
-    {
-      stepper2.setDirection(STOP);
-      return;
-    }
-     
-    stepper2.rotate(1);
-  }
-}
-
-void goLeft()
-{
-  motorLeft(-1);
-  motorRight(1);
-}
-
-void goRight()
-{
-  motorLeft(1);
-  motorRight(-1);
-}
-
-void driveForwards()
-{
-  //Serial.println("Forwards");
-  
-  motorLeft(-1);
-  motorRight(1);
-}
-
-void driveBackwards()
-{
-  //Serial.println("Backwards");
-  
-  motorLeft(1);
-  motorRight(-1);
-}
-
-void driveStop()
-{
-  motorLeft(0);
-  motorRight(0);
-}
-
